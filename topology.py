04389f1af78c25cebfa7bb64698532e12a63e55b
lucaboschiero <117825022+lucaboschiero@users.noreply.github.com>
2023-02-14 11:26:39 +0100

    Update topology.py

--- topology.py
+++ topology.py
@@ -1,1 +1,61 @@
-top
+"""Custom topology example
+
+Two directly connected switches plus a host for each switch:
+
+   host --- switch --- switch --- host
+
+Adding the 'topos' dict with a key/value pair to generate our newly defined
+topology enables one to pass in '--topo=mytopo' from the command line.
+"""
+
+from mininet.topo import Topo
+
+class MyTopo( Topo ):
+    "Simple topology example."
+
+    def build( self ):
+        "Create custom topo."
+
+        # Add hosts and switches
+        leftHost = self.addHost( 'h1' )
+        rightHost = self.addHost( 'h2' )
+        leftSwitch = self.addSwitch( 's3' )
+        rightSwitch = self.addSwitch( 's4' )
+
+        # Add links
+        self.addLink( leftHost, leftSwitch )
+        self.addLink( leftSwitch, rightSwitch )
+        self.addLink( rightSwitch, rightHost )
+
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
+
+if __name__ == "__main__":
+    topo = MyTopo()
+    net = Mininet(
+        topo=topo,
+        
+        # We specify an external controller by passing the Controller object in the Mininet constructor
+        # This was added in Mininet 2.2.0 and above.
+        # SOS Note: Do not specify port -- Default: 6653
+        controller=RemoteController( 'c0', ip='127.0.0.1'), 
+        switch=OVSKernelSwitch,
+        build=False,
+        autoSetMacs=True,
+        autoStaticArp=True,
+        link=TCLink,
+    )
+    
+    # ------------ Check Constructor for Controller ----------------- #
+    #controller = RemoteController("c1", ip="127.0.0.1", port=6633)
+    #net.addController(controller)
+    
+    net.build()
+    net.start()
+    
+    # Here we automate the process of creating the 2 slices by calling as a subprocess the common_scenario.sh
+    # Assumption: We begin with a non-emergency scenario.
+    subprocess.call("./common_scenario.sh")
+    
+    CLI(net)
+    net.stop()
