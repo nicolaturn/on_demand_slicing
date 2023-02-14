ea7414e1fff6563dde6b191eee6a6f9f2a29ce26
lucaboschiero <117825022+lucaboschiero@users.noreply.github.com>
2023-02-14 19:19:50 +0100

    Update initial_scenario.sh

--- initial_scenario.sh
+++ initial_scenario.sh
@@ -17,7 +17,7 @@ sudo ovs-ofctl add-flow s1 in_port=4,nw_dst=10.0.0.8, actions=output:1
 sudo ovs-ofctl add-flow s1 in_port=4,nw_dst=10.0.0.9, actions=output:2
 sudo ovs-ofctl add-flow s1 in_port=4,nw_dst=10.0.0.10, actions=output:2
 sudo ovs-ofctl add-flow s1 in_port=4,nw_dst=10.0.0.11, actions=output:2
-sudo ovs-ofctl add-flow s1 in_port=10.0.0.2,nw_dst=10.0.0.1, actions=output:3
+sudo ovs-ofctl add-flow s1 nw_src=10.0.0.2,nw_dst=10.0.0.1, actions=output:3
 sudo ovs-ofctl add-flow s1 in_port=3,nw_dst=10.0.0.3, actions=output:5
 sudo ovs-ofctl add-flow s1 in_port=3,nw_dst=10.0.0.4, actions=output:6
 sudo ovs-ofctl add-flow s1 in_port=3,nw_dst=10.0.0.5, actions=output:7
