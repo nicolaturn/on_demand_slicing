12413f904dfef8557dad960f10104103a03810de
lucaboschiero <117825022+lucaboschiero@users.noreply.github.com>
2023-02-17 12:34:34 +0100

    Update delete_slices.sh

--- delete_slices.sh
+++ delete_slices.sh
@@ -2,20 +2,20 @@
 
 if [ $1 = '1' ]
 then
-   sudo ovs-vsctl destroy queue --name @1q
+   sudo ovs-vsctl destroy queue -- --name @1q
 fi
 
 if [ $1 = '2' ]
 then
-   sudo ovs-vsctl destroy queue --name @2q
+   sudo ovs-vsctl destroy queue -- --name @2q
 fi
 
 if [ $1 = '3' ]
 then
-   sudo ovs-vsctl destroy queue --name @3q
+   sudo ovs-vsctl destroy queue -- --name @3q
 fi
 
 if [ $1 = '4' ]
 then
-   sudo ovs-vsctl destroy queue --name @4q
+   sudo ovs-vsctl destroy queue -- --name @4q
 fi
