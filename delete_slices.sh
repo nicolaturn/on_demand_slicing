036c42d50c2bd2b214ecf08a751c3d1b1ee180f6
lucaboschiero <117825022+lucaboschiero@users.noreply.github.com>
2023-02-17 13:22:37 +0100

    Update delete_slices.sh

--- delete_slices.sh
+++ delete_slices.sh
@@ -2,7 +2,9 @@
 
 if [ $1 = '1' ]
 then
-   sudo ovs-ofctl delete-flows s1 "actions=slice1"
+   ovs-ofctl del-flows s1 priority=64500
+   ovs-ofctl del-flows s2 priority=64500
+
    sudo ovs-ofctl delete-flows s2 "actions=slice1"
 fi
 
