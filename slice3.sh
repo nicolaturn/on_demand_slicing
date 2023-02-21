263b1a3321f4b5ba381263263b1f9d2d3538144f
lucaboschiero <117825022+lucaboschiero@users.noreply.github.com>
2023-02-21 20:21:42 +0100

    Update slice3.sh

--- slice3.sh
+++ slice3.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 
-echo '---------- Creating Slice 3 ----------'
+if [ -z "$1" ]
+then
+echo '---------- Creating Slice 1 ----------'
 echo 'Switch 1:'
+fi
 sudo ovs-vsctl set port s1-eth2 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=10000000 \
@@ -15,7 +18,10 @@ other-config:max-rate=10000000 \
 queues:3=@3q -- \
 --id=@3q create queue other-config:min-rate=1000000 other-config:max-rate=4000000
 
-echo 'Switch2:'
+if [ -z "$1" ]
+then
+echo 'Switch 2:'
+fi
 sudo ovs-vsctl set port s2-eth1 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=10000000 \
@@ -28,7 +34,10 @@ other-config:max-rate=10000000 \
 queues:3=@3q -- \
 --id=@3q create queue other-config:min-rate=1000000 other-config:max-rate=40000000 
 
-echo 'Switch3:'
+if [ -z "$1" ]
+then
+echo 'Switch 3:'
+fi
 sudo ovs-vsctl set port s3-eth1 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=10000000 \
@@ -40,7 +49,10 @@ sudo ovs-vsctl set port s3-eth2 qos=@newqos -- \
 other-config:max-rate=10000000 \
 queues:3=@3q -- \
 --id=@3q create queue other-config:min-rate=1000000 other-config:max-rate=40000000 
+if [ -z "$1" ]
+then
 echo '---------- End Creating Sice ----------'
+fi
 
 #mapping s1 queues to hosts (h3 - h11,h8)
 sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.3,nw_dst=10.0.0.8,idle_timeout=0,actions=set_queue:3,normal
